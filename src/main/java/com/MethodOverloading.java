b4ee075fdd8a918b998fea1808726cd22cac574d
sharm <nitin.rebelee91@gmail.com>
2019-05-26 10:16:34 +0530

    new code

---
+++ src/main/java/com/MethodOverloading.java
@@ -0,0 +1,19 @@
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+        System.out.println(getInt(1));
+    }
+
+    public static Object getInt(int a) {
+        return a;
+    }
+
+    public static float getInt(float a) {
+        return a;
+    }
+
+    public static double getInt(double a) {
+        return a;
+    }
+
+}
