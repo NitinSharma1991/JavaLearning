799b5a3f3f4d280a048d4136be2c08b447c2decb
sharm <nitin.rebelee91@gmail.com>
2019-05-26 10:17:23 +0530

    two changes

--- src/main/java/com/Anargam.java
+++ src/main/java/com/Anargam.java
@@ -13,6 +13,11 @@ public class Anargam {
         String s2 = sort(s);
         String s3 = sort(s1);
         int i = s2.compareTo(s3);
+
+        byte b =  1;
+        byte c = 100;
+        int d =  (b + c);
+        System.out.println(b + c + " " + d);
         System.out.println(i);
     }
 
--- src/main/java/com/MethodOverloading.java
+++ src/main/java/com/MethodOverloading.java
@@ -1,3 +1,5 @@
+package com;
+
 public class MethodOverloading {
 
     public static void main(String[] args) {
