e0ea565d6e2b9b7d12ade2c58b46b36273510a46
sharm <nitin.rebelee91@gmail.com>
2019-05-27 21:49:38 +0530

    two changes

--- src/main/java/com/DataStructure/Main.java
+++ src/main/java/com/DataStructure/Main.java
@@ -3,7 +3,7 @@ package com.DataStructure;
 public class Main {
 
     public static void main(String[] args) {
-        Stacks stacks = new Stacks();
+        Stacks stacks = new Stacks(10);
         if (!stacks.isFull()) {
             stacks.push(2);
             stacks.push(4);
@@ -13,8 +13,8 @@ public class Main {
             stacks.push(8);
             stacks.push(9);
         }
-      while (!stacks.isEmpty())
-        System.out.println(stacks.pop());
+        while (!stacks.isEmpty())
+            System.out.println(stacks.pop());
 
     }
 }
