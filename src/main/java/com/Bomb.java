c4aad7020a9c4d3fda0dfb7b0724823de12a2408
sharm <nitin.rebelee91@gmail.com>
2019-07-15 12:27:12 +0530

    two changes

---
+++ src/main/java/com/Bomb.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bomb {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String read[] = reader.readLine().split("\\s");
+
+
+    }
+
+
+}
