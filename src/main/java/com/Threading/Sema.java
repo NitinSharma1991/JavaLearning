0e61905d9d1d012ad66ef079820e913c76a4e35b
sharm <nitin.rebelee91@gmail.com>
2020-03-21 17:42:12 +0530

    Semaphore

--- src/main/java/com/Threading/Sema.java
+++ src/main/java/com/Threading/Sema.java
@@ -1,6 +1,5 @@
 package com.Threading;
 
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
@@ -9,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Sema {
 
     public static void main(String[] args) {
-        List webDriverList;
+
         ExecutorService service = Executors.newFixedThreadPool(4);
         Semaphore semaphore = new Semaphore(2);
         AtomicInteger atomicInteger = new AtomicInteger(0);
