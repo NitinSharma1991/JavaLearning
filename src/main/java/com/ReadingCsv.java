f22092d8207d68237e4a34d1fa411ffcaaae4e63
sharm <nitin.rebelee91@gmail.com>
2020-03-22 12:14:11 +0530

    Reading CSV

--- src/main/java/com/ReadingCsv.java
+++ src/main/java/com/ReadingCsv.java
@@ -1,9 +1,9 @@
 package com;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ReadingCsv {
 
@@ -12,13 +12,37 @@ public class ReadingCsv {
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader reader = new BufferedReader(new FileReader("C:\\Intellij Code\\JavaLearning\\src\\main\\resources\\Item.csv"));
         String row = null;
+        File file = new File("C:\\Intellij Code\\JavaLearning\\src\\main\\resources\\NewItem.csv");
+        List list = new ArrayList();
+        if (!file.exists()) file.createNewFile();
         while ((row = reader.readLine()) != null) {
             assert row != null;
             String[] read = row.split(",");
+            list.add(read);
             System.out.println(Arrays.toString(read));
         }
 
+        FileWriter fw = new FileWriter(file);
+        list.forEach(a -> {
+            try {
 
+                String[] b = (String[]) a;
+                StringBuilder builder = new StringBuilder();
+                builder.append(b[0]);
+                builder.append(",");
+                builder.append(b[1]);
+                builder.append(",");
+                builder.append(b[2]);
+
+                fw.append(builder);
+                fw.append("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        reader.close();
+        fw.flush();
+        fw.close();
     }
 
 
