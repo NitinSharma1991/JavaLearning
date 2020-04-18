d5df1e61e0af98b24efcd55119600e4f9b990c71
sharm <nitin.rebelee91@gmail.com>
2020-04-18 22:52:17 +0530

    Streams

--- src/main/java/com/hackerRank/queues/BalancedBrackets.java
+++ src/main/java/com/hackerRank/queues/BalancedBrackets.java
@@ -1,22 +1,36 @@
 package com.hackerRank.queues;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BalancedBrackets {
     private static final Scanner scanner = new Scanner(System.in);
 
     // Complete the isBalanced function below.
     static String isBalanced(String s) {
-        Map map = new HashMap();
-        map.put('{', '}');
-        map.put('(', ')');
-        map.put('[', ']');
-        for (int i = 0; i < s.length() / 2; i++) {
-            Character s1 = (Character) map.get(s.charAt(i));
-            if (!(s1 == s.charAt(s.length() - 1 - i))) return "NO";
+
+        Stack stack = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty()) return "NO";
+                char a = 0;
+
+                a = (char) stack.pop();
+
+                if (a == '(') {
+                    if (!(s.charAt(i) == ')')) return "NO";
+                }
+                if (a == '{') {
+                    if (!(s.charAt(i) == '}')) return "NO";
+                }
+                if (a == '[') {
+                    if (!(s.charAt(i) == ']')) return "NO";
+                }
+            }
         }
 
         return "YES";
@@ -24,19 +38,13 @@ public class BalancedBrackets {
 
     public static void main(String[] args) throws IOException {
 
-
         int t = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         for (int tItr = 0; tItr < t; tItr++) {
             String s = scanner.nextLine();
-
             String result = isBalanced(s);
-
             System.out.println(result);
         }
-
-
         scanner.close();
     }
 }
